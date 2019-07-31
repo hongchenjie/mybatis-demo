41912de16166eb74271518a531b3b0737b470046
lipo <you@example.com>
2019-07-31 10:16:58 +0800

    log4j2分环境打印日志，可以打印sql

--- src/main/java/org/study/mybatis/controller/UserController.java
+++ src/main/java/org/study/mybatis/controller/UserController.java
@@ -1,6 +1,7 @@
 package org.study.mybatis.controller;
 
 import com.github.pagehelper.PageInfo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,15 @@ import org.study.mybatis.util.PageForm;
 
 @RestController
 @RequestMapping("user")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
 
-    //
+    //http://localhost:8080/user/list?pageNum=1&pageSize=2
     @RequestMapping("list")
     public Object list(PageForm form) {
+        log.info(form.toString());
         PageInfo pageInfo = userService.list(form);
         return pageInfo;
     }
